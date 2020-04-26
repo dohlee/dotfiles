d7fd19165c8e60f95e0263cc604066e0a68a89a3
dohlee <dohlee.bioinfo@gmail.com>
2020-04-26 12:37:25 +0900

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+cp .vimrc ~/.vimrc
+cp .zshrc ~/.zshrc
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+source ~/.zshrc
